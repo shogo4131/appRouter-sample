afa0ccdf7048cd9586c37930ce505e5fa1b0695e
shogo4131 <shougo08697118@gmail.com>
2023-04-19 00:29:32 +0900

    feat: configファイルの追加

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+export const API_URL = process.env.UPSTASH_REDIS_REST_URL || '';
+export const API_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN || '';
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '';
+export const GOOGLE_CLIENT_SERCRET = process.env.GOOGLE_CLIENT_SERCRET || '';

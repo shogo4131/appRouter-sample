c8836c3567fb0a4248d9cf50fcc10b1191fdf77b
shogo4131 <shougo08697118@gmail.com>
2023-04-19 23:10:49 +0900

    feat: 認証機能を追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,16 +3,26 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 
+import { signIn } from 'next-auth/react';
+
 import GoogleIcon from '../../../public/google.svg';
 import { Button } from '../../components/Elements/Button';
 
 import styles from './page.module.css';
 
 const Login: NextPage = () => {
+  const onClickLoginHandler = async () => {
+    try {
+      await signIn('google');
+    } catch (error) {
+      throw new Error('Auth Login Error');
+    }
+  };
+
   return (
     <div className={styles.root}>
       <h1 className={styles.title}>ログイン</h1>
-      <Button type="button" className={styles.button}>
+      <Button type="button" onClick={onClickLoginHandler} className={styles.button}>
         <Image src={GoogleIcon} alt="google" />
         <span>Google</span>
       </Button>
